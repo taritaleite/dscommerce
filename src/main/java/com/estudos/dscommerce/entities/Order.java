de9066c6634f76d2095f2533ce79d09d6b98e1a1
taritaleite <taritaleite@gmail.com>
2025-01-19 19:20:28 -0300

    add: criacao de entidades

---
+++ src/main/java/com/estudos/dscommerce/entities/Order.java
@@ -0,0 +1,70 @@
+package com.estudos.dscommerce.entities;
+
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    //relacionamento muitos para um com a classe User
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Order setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public Order setMoment(Instant moment) {
+        this.moment = moment;
+        return this;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public Order setStatus(OrderStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public Order setClient(User client) {
+        this.client = client;
+        return this;
+    }
+}
---
+++ src/main/java/com/estudos/dscommerce/entities/User.java
@@ -0,0 +1,95 @@
+package com.estudos.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+    //relacionamento um para muitos com a classe Order
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders  = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public User setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public LocalDate getbirthDate() {
+        return birthDate;
+    }
+
+    public User setbirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+}
