f0a160fca89bc56e548075d7a628d236cf89e9e6
Tarita <taritaleite@gmail.com>
2025-04-28 21:20:10 -0300

    ajuste data base

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category(name) VALUES ('Livros')
+INSERT INTO tb_category(name) VALUES ('Eletrônicos')
+
+INSERT INTO tb_product(name, description, price, img_url) VALUES ('The Lord of the Rings', 'A fantasy novel by J.R.R. Tolkien', 29.99, 'https://example.com/lord_of_the_rings.jpg')
