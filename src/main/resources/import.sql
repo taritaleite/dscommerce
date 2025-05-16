bf4d906ac0b80ee67f12eee1e65a5898b4902287
Tarita <taritaleite@gmail.com>
2025-05-16 10:40:43 -0300

    inclusao de dados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
 INSERT INTO tb_category(name) VALUES ('Livros')
 INSERT INTO tb_category(name) VALUES ('Eletrônicos')
 
-INSERT INTO tb_product(name, description, price, img_url) VALUES ('The Lord of the Rings', 'A fantasy novel by J.R.R. Tolkien', 29.99, 'https://example.com/lord_of_the_rings.jpg')
+INSERT INTO tb_product(name, description, price, img_url) VALUES ('The Lord of t123
+he Rings', 'A fantasy novel by J.R.R. Tolkien', 29.99, 'https://example.com/lord_of_the_rings.jpg')
